c3b147306693cad95f273972042785c305655958
jusiro <jusiro95@gmail.com>
2023-08-16 10:45:55 -0400

    Re-organization of the code to modularize FLAIR

--- flair/modeling/constants.py
+++ flair/modeling/constants.py
@@ -1,5 +1,5 @@
 
-PATH_PRETRAINED_WEIGHTS = "./flair_pretrained_weights/"
+PATH_PRETRAINED_WEIGHTS = "./flair/modeling/flair_pretrained_weights/"
 
 ID_FLAIR_RESNET_V1 = "flair_resnet.pth"
 URL_ID_FLAIR_RESNET_V1 = "1l24_2IzwQdnaa034I0zcyDLs_zMujsbR"
